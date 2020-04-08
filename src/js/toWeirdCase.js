72cfe34b3db8d74b0732148faf6de717617daa39
Maxim Kremnev <maxim.kremnev@itqc.ru>
2020-04-08 12:01:46 +0300

    Функция Uppercase Char to String

---
+++ src/js/toWeirdCase.js
@@ -0,0 +1,15 @@
+//Хорошие решения
+// const toWeirdCase = (str) => str.replace(/\b\w*\b/g, (w) => w.replace(/\w/g, (c, k) => c[k % 2 === 0 ? 'toUpperCase' : 'toLowerCase']()));
+
+function toWeirdCase(string) {
+  let indexChar = 0;
+  let uppString = [];
+  let newString = string.split('');
+  newString.forEach((el) => {
+    indexChar % 2 == 0 && el != ' ' ? uppString.push(el.toUpperCase()) : uppString.push(el);
+    el != ' ' ? indexChar++ : (indexChar = 0);
+  });
+  return uppString.join('');
+}
+
+module.exports = toWeirdCase;
---
+++ test/toWeirdCase.test.js
@@ -0,0 +1,6 @@
+const toWeirdCase = require('../src/js/toWeirdCase');
+
+test('Парсинг верный', () => {
+  expect(toWeirdCase('Weird string case')).toMatch(/WeIrD StRiNg CaSe/);
+  expect(toWeirdCase('String')).toMatch(/StRiNg/);
+});
