3fab9ad643d1d5c2d16b22c23985f0296b6344eb
Maxim Kremnev <m.kremnev@netlinux.ru>
2021-12-30 10:41:21 +0300

    Updated config jest with babel

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
-const config = {
+module.exports = {
   verbose: true,
   rootDir: ".",
   coverageDirectory: "coverage",
-  transform: {},
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  "moduleFileExtensions": ["js"],
   moduleNameMapper: {
     // https://jestjs.io/docs/en/webpack#handling-static-assets
     "^@/(.*)$": "<rootDir>/src/$1",
   },
   moduleDirectories: ["node_modules", "src", "test"],
+  testRegex: "((\\.|/*.)(test))\\.js?$",
   coverageThreshold: {
     global: {
       branches: 75,
@@ -17,5 +21,3 @@ const config = {
     },
   },
 };
-
-export default config;
